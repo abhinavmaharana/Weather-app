515bb4e75ebd31de981a606168dac4b8d32a9ccb
ABHINAV MAHARANA <abhinavmaharana.004@gmail.com>
2022-07-13 10:26:19 +0530

    🐛 deployment

--- backend/app.js
+++ backend/app.js
@@ -27,13 +27,13 @@ const weather = require("./routes/weatherRoute");
 
 app.use("/api/weather", weather);
 
-if (process.env.NODE_ENV== 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname,"frontend",'build')))
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", 'build', 'index.html'));
+
+    app.get("/", (req, res) => {
+        app.use(express.static(path.join(__dirname,"../frontend/build")))
+        res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
     });
-}
+
 
 module.exports = app;
 
